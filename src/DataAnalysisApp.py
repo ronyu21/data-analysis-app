31287198fb55ecc6ea6401052ba43ad0338bb846
Ron Yu <ron.jhyu@outlook.com>
2019-11-27 23:24:10 -0700

    completed day 2 assignment

--- src/DataAnalysisApp.py
+++ src/DataAnalysisApp.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from src.FileHandling import load_csv_to_2d_list
 
 if __name__ == "__main__":
@@ -15,8 +13,49 @@ if __name__ == "__main__":
         value = item[1:]
         data_dict.setdefault(key, value)
 
-    # output the dictionary as shown in the email
+    # output the dictionary as shown in the email day 1
     # key - value
-    for key, val in data_dict.items():
-        print(key, ' - ', val)
+    # for key, val in data_dict.items():
+    #     print(key, ' - ', val)
 
+    # day 2
+    print("All data from Emissions.csv has been read into a dictionary\n")
+    # wait for user input
+    target_year = input("Select a year to find statics (1997 to 2010): ")
+
+    # find the index of the user input
+    header_row_key = "CO2 per capita"
+    index = None
+    years = data_dict.get(header_row_key)
+    temp_idx = 0
+    for year in years:
+        if target_year == year:
+            index = temp_idx
+            break
+        temp_idx += 1
+
+    if index is None:
+        print("User entered an invalid year: {}".format(target_year))
+        exit(-1)
+
+    # extract the emissions for the target year
+    target_year_emissions = []
+    target_year_emission_countries = []
+    for k, v in data_dict.items():
+        if k == header_row_key:
+            continue
+        else:
+            target_year_emissions.append(float(v[index]))
+            target_year_emission_countries.append(k)
+
+    # calculate the min, max, average
+    min_val = min(target_year_emissions)
+    min_val_country = target_year_emission_countries[target_year_emissions.index(min_val)]
+    max_val = max(target_year_emissions)
+    max_val_country = target_year_emission_countries[target_year_emissions.index(max_val)]
+    avg_val = sum(target_year_emissions) / len(target_year_emissions)
+
+    # output the result as shown in email
+    print('In {}, countries with minimum and maximum CO2 emission levels were [{}] and [{}] respectively.'.format(
+        target_year, min_val_country, max_val_country))
+    print('Average CO2 emissions in {} were {:0.6f}'.format(target_year, avg_val))
